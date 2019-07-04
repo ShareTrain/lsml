ac41d5ffcfb4e94701326e19c72ab1db2d233b2f
Rushing <liming20110711@163.com>
2019-07-05 00:50:25 +0800

    Initial Commit

---
+++ lsml-server/src/main/java/com/lisong/common/param/ParamRepository.java
@@ -0,0 +1,8 @@
+package com.lisong.common.param;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParamRepository extends JpaRepository<Param, Long> {
+
+    Param findByParamCodeAndDeleted(String paramCode, Integer deleted);
+}
---
+++ lsml-server/src/main/java/com/lisong/common/param/po/BaseParamPO.java
@@ -0,0 +1,11 @@
+package com.lisong.common.param.po;
+
+import java.io.Serializable;
+
+public interface BaseParamPO extends Serializable {
+
+    /** 参数是否启用. */
+    boolean isEnabled();
+
+    void setEnabled(boolean enabled);
+}
